45eaef72c8e5b521315f0f1ce16a1ee6017bde26
John Deen Bucog <JDBucog@yahoo.com>
2022-07-25 19:27:54 -0700

    short circut str length anagram

--- is-anagram/isAnagram.js
+++ is-anagram/isAnagram.js
@@ -11,6 +11,7 @@ function FreqMap(str) {
 
 function isAnagram(str1, str2) {
   if ((!str1 && str2) || (str1 && !str2)) return false
+  if (str1.length !== str2.length) return false
   let str1Lower = str1.toLowerCase()
   let str2Lower = str2.toLowerCase()
   const str1Map = new FreqMap(str1Lower)
