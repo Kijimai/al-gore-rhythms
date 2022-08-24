d9a4817a14e51d74046b0d90660ad91afdc5fc82
John Deen Bucog <JDBucog@yahoo.com>
2022-08-24 15:20:53 -0700

    bubble sort optimized

--- bubble-sort/bub1.js
+++ bubble-sort/bub1.js
@@ -1,7 +1,8 @@
 // time - O(n^2)
 
 const numArray = [5, 7, 12, 1, 2, 4, 6]
-
+const numArray2 = [1231, 42, 45, 5, 1, 43, 66, 98, 32, 11]
+const numArray3 = [8, 1, 2, 3, 4, 5, 6, 7]
 //unoptimized
 // function bubble(arr) {
 //   for (let i = 0; i < arr.length; i++) {
@@ -16,14 +17,32 @@ const numArray = [5, 7, 12, 1, 2, 4, 6]
 //   return arr
 // }
 
+// function bubble(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       // console.log(`[${arr}], ${arr[j]} ${arr[j + 1]}`)
+//       if (arr[j] > arr[j + 1]) {
+//         swap(arr, j, j + 1)
+//       }
+//     }
+//   }
+//   return arr
+// }
+
+// More Optimized to check if a swap occured, if not -- stop
+// Best for "nearly sorted" arrays -- to avoid having to re-sort
 function bubble(arr) {
+  let swapped
   for (let i = arr.length; i > 0; i--) {
+    swapped = true
     for (let j = 0; j < i - 1; j++) {
-      // console.log(`[${arr}], ${arr[j]} ${arr[j + 1]}`)
+      console.log(`[${arr}], ${arr[j]} ${arr[j + 1]}`)
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1)
+        swapped = false
       }
     }
+    if (!swapped) break
   }
   return arr
 }
@@ -34,4 +53,6 @@ function swap(arr, idx1, idx2) {
   arr[idx2] = temp
 }
 
-console.log(bubble(numArray))
+// console.log(bubble(numArray))
+// console.log(bubble(numArray2))
+console.log(bubble(numArray3))
