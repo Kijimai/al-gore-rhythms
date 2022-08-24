7e42e4d89ad8c1f2f5102fecefb513894cdf93e7
John Deen Bucog <JDBucog@yahoo.com>
2022-08-24 15:37:50 -0700

    ... redid logic

--- bubble-sort/bub1.js
+++ bubble-sort/bub1.js
@@ -34,15 +34,15 @@ const numArray3 = [8, 1, 2, 3, 4, 5, 6, 7]
 function bubble(arr) {
   let swapped
   for (let i = arr.length; i > 0; i--) {
-    swapped = true
+    swapped = false
     for (let j = 0; j < i - 1; j++) {
       console.log(`[${arr}], ${arr[j]} ${arr[j + 1]}`)
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1)
-        swapped = false
+        swapped = true
       }
     }
-    if (swapped) break
+    if (!swapped) break
   }
   return arr
 }
